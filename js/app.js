0e0c07ccd6817e9f82466428000e2a20769103dc
n8kolenberg <nkolenberg@gmail.com>
2015-08-13 23:13:53 +0100

    Started refactoring portfolio and added a js plugin to showcase portfolio

---
+++ js/app.js
@@ -0,0 +1,42 @@
+$(function(){
+	
+// init cubeportfolio
+    $('#js-grid-full-width').cubeportfolio({
+        layoutMode: 'mosaic',
+        sortToPreventGaps: true,
+        defaultFilter: '*',
+        animationType: 'fadeOutTop',
+        gapHorizontal: 0,
+        gapVertical: 0,
+        gridAdjustment: 'responsive',
+        mediaQueries: [{
+            width: 1500,
+            cols: 5
+        }, {
+            width: 1100,
+            cols: 4
+        }, {
+            width: 800,
+            cols: 3
+        }, {
+            width: 480,
+            cols: 2
+        }, {
+            width: 320,
+            cols: 1
+        }],
+        caption: 'zoom',
+        displayType: 'lazyLoading',
+        displayTypeSpeed: 100,
+
+        // lightbox
+        lightboxDelegate: '.cbp-lightbox',
+        lightboxGallery: true,
+        lightboxTitleSrc: 'data-title',
+        lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+    });
+
+
+
+
+}); //End ready
