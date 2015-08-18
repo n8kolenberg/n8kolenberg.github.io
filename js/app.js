5f748c16a73c490380e174fba7eb8fcf6f36c06b
n8kolenberg <nkolenberg@gmail.com>
2015-08-18 22:51:39 +0100

    Added sticky nav, added new styling for masthead, added up and down arrows, added smooth scrolling

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 // $(function(){
 
 
+$('#nav').on('sticky-start', function() {
+  $(this).css({
+    "background-color": "rgba(246, 246, 246, 0.95)",
+    "z-index": 100
+  });
+}) //End on sticky-start
+.on('sticky-end', function() {
+    $(this).css({
+        "background-color": "rgba(246, 246, 246, 0.5)"
+    });
+}); //End on sticky-end
+
+
+$('#nav').sticky();
+
+
+
 // init cubeportfolio
      $('#js-grid-awesome-work').cubeportfolio({
         filters: '#js-filters-grid-awesome-work',
@@ -31,6 +48,25 @@
         displayTypeSpeed: 100,
     });
 
+        $(document).on("click", "#nav li a, .icon-down-open, .icon-up-open", function(event){        
+            event.preventDefault();
+        
+            //This should set the variable "page" as either #home, #profile, #portfolio, or #contact:
+            if( $(this).attr("href") ) {
+                var page = $(this).attr("href");
+            }
+            else {
+                var page = $(this).closest("a").attr("href");
+            }
+                
+            //For soft-scrolling:       
+            if( $("header").css("display") === "none" ) {
+                $("html, body").animate({ scrollTop: parseInt( $(page).position().top )}, 1000);  //Soft-scroll for mobile devices.
+            }
+            else {
+                $("html, body").animate({ scrollTop: parseInt( $(page).position().top ) - 50 }, 1000);  //Soft-scroll for Desktops. (Accounts for fixed menu at the top.)
+            }   
+    }); // End on click
 
 
 // }); //End ready
